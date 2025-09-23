21e19f3b292161fe5681661ce27313f89f2c259a
Dev <dev.data.collection@gmail.com>
2025-09-23 12:39:38 -0300

    Add styles iniciado

--- src/components/ApplicationsTable.jsx
+++ src/components/ApplicationsTable.jsx
@@ -19,7 +19,7 @@ export default function ApplicationsTable({ asignados, now }) {
         {asignados.map((registro, index) => {
           console.log(registro)
           const estado = calcularEstado(registro.horareingresoestimada, now, registro.estado)
-
+          console.log(estado)
           return (
             <tr key={index}>
               <td>{registro.dia_panificada}</td>
