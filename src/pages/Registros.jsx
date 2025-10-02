0584e4a0d1e16404a66bf341cdbd6e23d82b3482
devdatacoll <dev.data.collection@gmail.com>
2025-10-02 14:48:53 -0300

    fIX 4

--- src/pages/Registros.jsx
+++ src/pages/Registros.jsx
@@ -36,12 +36,16 @@ function Registros() {
 
   */
   //-----
+  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+  const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
+  const supabase = createClient(supabaseUrl, supabaseKey)
+  /*
   const supabase = createClient(
     process.env.VITE_SUPABASE_URL,
     process.env.VITE_SUPABASE_ANON_KEY
   )
-
+*/
   //---------------
 /*
   useEffect(() => {
