9926a9dd616dad0244d45ede0f9bf7f5ce509da1
Dev <dev.data.collection@gmail.com>
2025-10-02 10:30:12 -0300

    Api 2

--- src/pages/Registros.jsx
+++ src/pages/Registros.jsx
@@ -44,7 +44,7 @@ const [asignados, setAsignados] = useState([])
       const res = await fetch(`https://yxzevjktytzjeszqeojt.supabase.co/rest/v1/view_asignados_today?select=*`, {
         headers: {
           Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
-          apikey: `${import.meta.env.VITE_API_TOKEN}`
+          apikey: `${import.meta.env.VITE_API_APIK}`
         },
       });
 
